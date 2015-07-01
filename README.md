bb863243ff815fdd171f1b656e501eb32ccae193
Dustin Moore <dustmoo@gmail.com>
2015-06-30 17:03:21 -0700

    Switch to ember flavored blueprints, (better json structure)

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # sourcecheck_api
-Primary Repo for the Sourcecheck API.
+Official Repo for the Sourcecheck API.
 
-ToDo: Fill out the Readme.
+This is the repository for our server code.
 
-This is using the sails.js node framework, which has a robust MVC model and organization. It's documentation is awesome: http://sailsjs.org
+Currently we have a Source and and Organization object.
+
+We have a basic crud api working.
+
+## ToDo:
+
+Expand on ownership association chains.
+Create data import and feedback api.
+Prepair for front end plugin.
+
+To speed development we have chosen to use the sails.js node framework for our backend.
+Sails a robust MVC model and organization. It's documentation is awesome: http://sailsjs.org
 
 
--- api/controllers/SourceController.js
+++ api/controllers/SourceController.js
@@ -7,39 +7,6 @@
 
 module.exports = {
 	//Main info API Call
-
-	check: function(req,res) {
-		//To Do: Add error handling, work through deep assocations.
-		var host = req.param('host');
-		var to_send = {};
-
-		//Find source that matches host
-		Source.findOne({host: host}).populate("organizations").then(function(source){
-			source.owner_ids = [];
-			for (var i = source.organizations.length - 1; i >= 0; i--) {
-				source.owner_ids = source.organizations[i].id;
-			};
-			var orgOwners = Organization.find({
-				id: _.pluck(source.organizations, 'id')
-				//_.pluck: Retrieves the value of a 'user' property from all elements in the post.comments collection.
-			})
-			.then(function(orgOwners){
-/*
-				for (var i = orgOwners.length - 1; i >= 0; i--) {
-					orgOwners[i].owner_ids = [];
-					//Parse Owners
-					for (var o = orgOwners[i].owners.length - 1; o >= 0; o--) {
-						orgOwners[i].owner_ids.push(orgOwners[i].owners[o].id)
-					};
-				};*/
-
-
-				source.fixed_orgs = orgOwners;
-				res.send(200, source);
-				//var new_source = {organizations: orgOwners, host: source.host, createdAt: source.createdAt, updatedAt: source.updatedAt, id: source.id};
-				//res.send(200, new_source);
-			});
-		});
 	}
 	
 };
